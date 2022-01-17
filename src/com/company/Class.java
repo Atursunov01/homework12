c9e44eea0f15fe2db31c0050dd21abebe734df86
Elvira <akzholtursunov01@gmail.com>
2022-01-17 21:58:40 +0300

    Initial commit

---
+++ src/com/company/Class.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Class {
+     public static int san;
+     public static String soz;
+     int [] massiv ;
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      Class  Class = new Class();
+      Class.san = 15;
+      Class.soz = "Kyrgyzstan";
+      int [] massiv = {10};
+
+        System.out.println(Class.san);
+        System.out.println(Class.soz);
+        System.out.println(massiv);
+
+
+    }
+}
